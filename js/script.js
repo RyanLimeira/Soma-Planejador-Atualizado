479343df8297f7050f9cc8a655732a3b3bdf68e5
RyanLimeira <limeira.ryan1110@gmail.com>
2023-11-15 18:57:17 -0300

    Atualizações projeto

--- js/script.js
+++ js/script.js
@@ -1,75 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtém referências para elementos HTML
     const cadastroDespesa = document.getElementById("cadastroDespesa");
     const cadastroMetas = document.getElementById("cadastroMetas");
     const mensagemDespesa = document.getElementById("mensagemDespesa");
     const mensagemMetas = document.getElementById("mensagemMetas");
-    const listaDespesas = document.getElementById("containerDespesas");
     const listaMetas = document.getElementById("containerMetas");
+    const listaDespesasCadastro = document.getElementById("containerDespesasCadastro");
+    const listaDespesas = document.getElementById("containerDespesas");
+    const totalDespesasElement = document.getElementById("totalDespesas");
     const exibirDespesas = document.getElementById("exibirDespesas");
-    const exibirMetas = document.getElementById("exibirMetas");
 
-    // Event listener para o botão "Cadastrar Despesa"
     cadastroDespesa.addEventListener("click", function () {
-        // Obtém os valores dos campos de entrada
         const nomeDespesa = document.getElementById("nomeDespesa").value;
         const valorDespesa = document.getElementById("valorDespesa").value;
 
         if (nomeDespesa && valorDespesa) {
-            // Cadastrar a despesa e exibi-la na lista
             const divDespesa = document.createElement("div");
             divDespesa.textContent = `${nomeDespesa}: R$${valorDespesa}`;
-            listaDespesas.appendChild(divDespesa);
+            listaDespesasCadastro.appendChild(divDespesa);
 
-            // Limpar os campos de entrada e a mensagem de erro
             document.getElementById("nomeDespesa").value = "";
             document.getElementById("valorDespesa").value = "";
             mensagemDespesa.textContent = "";
         } else {
-            // Exibir mensagem de erro se os campos não estiverem preenchidos
             mensagemDespesa.textContent = "Preencha todos os campos para cadastrar a despesa.";
         }
     });
 
-    // Event listener para o botão "Cadastrar Metas Financeiras"
     cadastroMetas.addEventListener("click", function () {
-        // Obtém os valores dos campos de entrada
         const limiteGastos = document.getElementById("limiteGastos").value;
         const economizar = document.getElementById("economizar").value;
 
         if (limiteGastos && economizar) {
-            // Cadastrar a meta financeira e exibi-la na lista
             const divMetas = document.createElement("div");
             divMetas.textContent = `Limite de Gastos: R$${limiteGastos}, Economizar: R$${economizar}`;
             listaMetas.appendChild(divMetas);
 
-            // Limpar os campos de entrada e a mensagem de erro
             document.getElementById("limiteGastos").value = "";
             document.getElementById("economizar").value = "";
             mensagemMetas.textContent = "";
         } else {
-            // Exibir mensagem de erro se os campos não estiverem preenchidos
             mensagemMetas.textContent = "Preencha todos os campos para cadastrar a meta financeira.";
         }
     });
 
-    // Event listener para o botão "Exibir Despesas"
     exibirDespesas.addEventListener("click", function () {
-        // Alternar a exibição da lista de despesas
-        if (listaDespesas.style.display === "none" || listaDespesas.style.display === "") {
-            listaDespesas.style.display = "block";
-        } else {
-            listaDespesas.style.display = "none";
-        }
-    });
+        fetch("https://65550e2263cafc694fe761a7.mockapi.io/api/v1/despesa")
+            .then(response => response.json())
+            .then(data => {
+                listaDespesas.innerHTML = '';
+                data.forEach(despesa => {
+                    const divDespesa = document.createElement("div");
+                    divDespesa.textContent = `${despesa.nome}: R$${despesa.valor}`;
+                    listaDespesas.appendChild(divDespesa);
+                });
 
-    // Event listener para o botão "Exibir Metas Financeiras"
-    exibirMetas.addEventListener("click", function () {
-        // Alternar a exibição da lista de metas financeiras
-        if (listaMetas.style.display === "none" || listaMetas.style.display === "") {
-            listaMetas.style.display = "block";
-        } else {
-            listaMetas.style.display = "none";
-        }
+                const totalDespesas = data.reduce((total, despesa) => total + parseFloat(despesa.valor), 0);
+                totalDespesasElement.textContent = `Total: R$${totalDespesas.toFixed(2)}`;
+            })
+            .catch(error => {
+                console.error("Erro ao obter dados da API:", error);
+                mensagemDespesa.textContent = "Erro ao obter dados da API.";
+            });
     });
 });
